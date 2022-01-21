a13db715d6099ae302dcfbad5b4c7eb3801f6268
Justine Craig <justincrg@gmail.com>
2022-01-21 14:02:38 -0800

    Added ability to display and clear numbers and operators. Next, evaluation of expressions

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
-function Clicked(event) {
-    if(event.target.textContent == 'CE')
-    {
-        screenText = '';
-        screen.textContent = screenText;
+
+//Only handle integers for now, will deal with floats later
+function ClickedNum(event) {
+    let temp = parseInt(event.target.textContent);
+    currentNum *= 10;
+    currentNum += temp;
+    DisplayText();
+}
+
+function ClickedOp(event) {
+    //Can only add an op if there is a number for it to act on
+    //This breaks parenthesis, but they will be handled specially anyways
+    if (currentNum != 0) {
+        nums.push(currentNum);
+    }
+    currentNum = 0;
+    ops.push(event.target.textContent);
+    DisplayText();
+}
+
+function ClearAll(event) {
+    nums = [];
+    ops = [];
+    currentNum = 0;
+    DisplayText();
+}
+
+function DisplayText() {
+    let temp = ''
+    if (nums.length != ops.length) {
+        console.log("Length of numberlist and length of operationlist do not match");
+        screen.textContent = 'Error';
         return;
     }
-    screenText += event.target.textContent;
-    screen.textContent = screenText;
+    for (let i = 0; i < nums.length; i++) {
+        temp += nums[i].toString();
+        temp += ' ';
+        temp += ops[i];
+        temp += ' ';
+    }
+    if (currentNum != 0) {
+        temp += currentNum.toString();
+    }
+    screen.textContent = temp;
 }
 
-let screenText = '';
+let currentNum = 0;
+let nums = [];
+let ops = [];
 
-const buttons = document.querySelectorAll('button');
-const screen = document.querySelector('.screen');
 
-for(let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click',Clicked);
-}
+const numButtons = document.querySelectorAll('.num');
+const opButtons = document.querySelectorAll('.op');
+const screen = document.querySelector('#screen');
+const clear = document.querySelector('#clear');
+
+numButtons.forEach(item => { item.addEventListener('click', ClickedNum) })
+opButtons.forEach(item => { item.addEventListener('click', ClickedOp) })
+clear.addEventListener('click', ClearAll);
+
+DisplayText();
