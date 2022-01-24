8987d36924ceb68b7f819d6ff74f2879bc376886
Justine Craig <justincrg@gmail.com>
2022-01-24 11:43:19 -0800

    Changed number handling to use strings instead of ints to preserve leading zeros

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
 //Only handle integers for now, will deal with floats later
 function ClickedNum(event) {
-    let temp = parseInt(event.target.textContent);
+    //Could convert to an int here and track integers
+    //This leads to problems of dropping leading zeroes after a decimal place that should be displayed
+    //instead, process the numbers as strings until we are ready to run operators on them
+    //This should maintain leading zeroes.
+    let temp = event.target.textContent;
     if (currentNum == null) {
         currentNum = temp;
         DisplayText();
         return;
     }
-    currentNum *= 10;
+    //Concatenate numbers together rather than multiplying by ten and adding integers
     currentNum += temp;
     DisplayText();
 }
@@ -16,6 +20,7 @@ function ClickedOp(event) {
     //Can only add an op if there is a number for it to act on
     //This breaks parenthesis, but they will be handled specially anyways
     //However, it correctly displays and error if more than one operator is pressed in a row
+    //Adding two decimal operators to a single number should throw an error, but currently does not. Can check during op evaluation
     if (currentNum != null) {
         nums.push(currentNum);
     }
@@ -31,30 +36,47 @@ function ClickedClearAll(event) {
     DisplayText();
 }
 
+
+//The equals button should scan the current lest of numbers and operations, applying ops to the numbers
+//At the same index, and at index +1. Numbers will be replaced with the result and op will be removed
+//Numbers and ops are stored as strings, so conversion will be necessary
+//Order of operations will be maintained by checking the list of ops in order of priority
+//Order of Ops
+//Decimal Op - Create a floating point number out of two integers with decimal between
+//Parenthesis - Haven't figured this out yet
+//Multiplication and Division
+//Addition and Substraction
 function ClickedEquals(event) {
-    
+    if (currentNum == null) {
+        screen.textContent = 'Syntax Error, expression must end in a number';
+        return;
+    }
+
 }
 
 function DisplayText() {
     let temp = ''
     if (nums.length != ops.length) {
-        console.log("Length of numberlist and length of operationlist do not match");
-        screen.textContent = 'Error';
+        if (nums.length == 0) {
+            screen.textContent = 'Error, expression must start with a number';
+            return;
+        }
+        screen.textContent = 'Error, expression cannot contain consecutive operators';
         return;
     }
     for (let i = 0; i < nums.length; i++) {
-        temp += nums[i].toString();
+        temp += nums[i];
         temp += ' ';
         temp += ops[i];
         temp += ' ';
     }
     if (currentNum != null) {
-        temp += currentNum.toString();
+        temp += currentNum;
     }
     screen.textContent = temp;
 }
 
-let currentNum = 0;
+let currentNum = null;
 let nums = [];
 let ops = [];
 
